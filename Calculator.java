6e6fe2bae74cbccd6147734b12d94a1b18d8ac30
zraza2511 <145640338+zraza2511@users.noreply.github.com>
2023-10-13 10:27:46 +0530

    java practice

---
+++ Calculator.java
@@ -0,0 +1,14 @@
+class Calci {
+
+    public static void main(String args[]) {
+        int a = 10, b = 20;
+        System.out.println("Addition :" + (a + b));
+
+        System.out.println("Sub :" + (a - b));
+
+        System.out.println("Mul :" + (a * b));
+
+        System.out.println("Divi :" + (a / b));
+    }
+
+}
