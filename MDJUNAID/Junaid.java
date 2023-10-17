ede0aef01b05d5f8ae1bfe484c6e1691ae4b9b5b
smartjunaid <smartjunaid9823@gmail.com>
2023-10-17 11:29:28 +0530

    junaid

---
+++ MDJUNAID/Junaid.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Junaid{
+public static void main(String args[]){
+
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter First Value: ");
+int val1=sc.nextInt();
+System.out.println("Enter Second Value: ");
+int val2=sc.nextInt();
+System.out.println("Addition: "+(val1+val2));
+System.out.println("Substraction: "+(val1-val2));
+System.out.println("Multiplication: "+(val1*val2));
+System.out.println("Division: "+(val1/val2));
+
+}
+}
