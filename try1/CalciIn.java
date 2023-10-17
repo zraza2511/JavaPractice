69164e9c8d0cc6825fa1d93b3e8b626296cbf6a5
zraza2511 <145640338+zraza2511@users.noreply.github.com>
2023-10-17 10:56:35 +0530

    jhcwgefyiwge

---
+++ try1/CalciIn.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class CalciIn{
+
+public static void main(String args[]){
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter username");
+	
+	String userName = sc.nextLine();  
+    System.out.println("Username is: " + userName);  
+	
+	
+}
+
+}
