bc9026acd8824ccabe37d78549259ee86a13ab25
zraza2511 <145640338+zraza2511@users.noreply.github.com>
2023-10-18 10:35:36 +0530

    Inn WORD

---
+++ try1/Inn.java
@@ -0,0 +1,8 @@
+public class  Inn{
+  public static void main (String[] args) {
+    System.out.println(3);
+    System.out.println(358);
+    System.out.println(786786);
+    System.out.println(50000);
+  }
+}
