4a4b5bbd340ef0772cef5a7c8e803cfdf7fbef70
skjunaid2319 <145640633+skjunaid2319@users.noreply.github.com>
2023-10-30 15:58:28 +0530

    Add play file

---
+++ Junaiduddin/play.java
@@ -0,0 +1,46 @@
+public class play
+{
+public static void main (String args[]){
+{
+System.out.println("I PLAY CRICKET VERY WEll");
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
