971b3014384d7b7ee6193d2b906a1292106d23d4
skjunaid2319 <145640633+skjunaid2319@users.noreply.github.com>
2023-11-29 10:19:03 +0530

    data tyape

--- Junaiduddin/Test1.java
+++ Junaiduddin/Test1.java
@@ -1,4 +1,4 @@
-public class Test{
+public class Test1{
 public static void main(String args[]){
 
 System.out.print("im a full stack devolapar ");
---
+++ datatype/test.java
@@ -0,0 +1,42 @@
+package com.tw.datatype;
+
+
+public class test{
+public static void main(String[]args) {
+       System.out.println("hello ima junaid");
+       
+       boolean isaman=false;
+       if(isaman) {
+       System.out.println("man");
+       }else{
+       System.out.println("women");	   
+       
+       char myGrad='B';
+       System.out.println(myGrad);
+       
+       byte myNum=100;
+       System.out.println(myNum);
+       
+       short myPin=500;
+       System.out.println(myPin);
+       
+       int myMo =100000;
+       System.out.println(myMo);
+       
+       long myNumber =150000000000L;
+       System.out.println(myNumber);
+       
+       
+       float myNume =5.75f;
+       System.out.println(myNum);
+       
+       double myNimbers =19.99d;
+       System.out.println(myNimbers);
+       
+ }     
+       
+     
+       
+}
+}
+
